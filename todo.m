9c5ec149fa19d8fc8b9379520437b5fa2ff1911d
Alok Choudhary <alok.c83@gmail.com>
2014-04-22 00:21:08 -0400

    Imported user code snippets

---
+++ todo.m
@@ -0,0 +1,10 @@
+// TODO
+// Setting TODO with help of warning directive
+//
+// IDECodeSnippetCompletionPrefix: todo
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 5B0010D6-97CD-403A-B56C-34D2E98B4D99
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#warning method incomplete TODO :
---
+++ twitter_controller.m
@@ -0,0 +1,15 @@
+// Twitter Controller
+// Invoking twitter controller
+//
+// IDECodeSnippetCompletionPrefix: twitterController
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 61DA339B-1B90-4306-A9F7-331E12DC3147
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetPlatformFamily: iphoneos
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+ NSString *tweetText = [NSString stringWithFormat:@"%@",tweetString];
+SLComposeViewController *composer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+    [composer setInitialText:tweetText];
+    [self presentViewController:composer animated:YES completion:nil];
